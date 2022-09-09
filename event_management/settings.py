5bd422985fc9d953252f2fdd4b2b2e018440083d
vaibhav c <83589185+Vaibhav1934@users.noreply.github.com>
2022-09-09 12:25:29 +0530

    Update settings.py

--- event_management/settings.py
+++ event_management/settings.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 BASE_DI = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DI / 'static'
+#STATIC_ROOT = BASE_DI / 'static'
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
